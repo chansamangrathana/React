e7bd9fd89393b58a3a6ce7e01e0f599114d71461
chansamangrathana <chansamangrathana1607@gmail.com>
2024-03-06 23:23:58 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import NavbarComponet from './component/NavbarComponent'
+
+const App=()=> {
+  
+
+  return (
+    <>
+    <NavbarComponet/>
+    <h1>
+      hello
+    </h1>
+    </>
+  )
+}
+
+export default App
